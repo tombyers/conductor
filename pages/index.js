338ccbcf07df6a96993b0866adaa16678606225a
tombyers <tombyers@pm.me>
2020-06-06 12:28:35 +0100

    Set up with React hooks and Framer motion - woo

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
 import Head from 'next/head'
-import { motion } from 'framer-motion'
+import { useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
 
 const batonStyles = {
   height: 48,
   width: 48,
-  backgroundColor: 'rgba(0,0,0,1)',
   position: 'relative',
-  top: 0
+  backgroundColor: 'white',
+  top: 0,
+  borderRadius: 24,
+  boxShadow: '0 0 12px 4px rgba(0,0,0,1)'
 }
 
-const batonAnimation = {
-  backgroundColor: ['rgb(255,0,0)', 'rgb(0,0,0)', 'rgb(0,0,0)', 'rgb(0,0,0)', 'rgb(255,0,0)'],
-  x: [0, 100, -100, 0, 0],
-  y: [0,  0, 0, -100, 0],
-  transition: {
-    loop: Infinity,
-    duration: 2,
-    ease: [.6,.05,.67,1.12]
-  }
+const variants = {
+  playing: { 
+    opacity: 1,
+    x: [0, 100, -100, 0, 0],
+    y: [0,  0, 0, -100, 0],
+    transition: {
+      loop: Infinity,
+      duration: 2,
+      ease: [.6,.05,.67,1.12]
+    }
+  },
+  paused: { opacity: 0 }
+}
+
+const h1Style = {
+  margin: 0,
+  lineHeight: 1.15,
+  fontSize: '4rem',
+  textAlign: 'center',
+  paddingBottom: '8rem',
+  color: 'white'
+}
+
+const mainContent = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column'
+}
+
+
+const Conductor = () => {
+  const [ isPlaying, togglePlay ] = useState(false);
+  const controls = useAnimation()
+
+  return (
+    <main style={mainContent} onClick={() => togglePlay(!isPlaying)}>
+      <h1 style={h1Style}>Conductor</h1>
+      <motion.div style={batonStyles} initial="playing" animate={ isPlaying ? "playing" : "paused"} variants={variants}></motion.div>
+    </main>
+  )
 }
 
 export default function Home() {
@@ -28,12 +63,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Conductor
-        </h1>
-        <motion.div style={batonStyles} animate={batonAnimation}></motion.div>
-      </main>
+      <Conductor />
 
       <style jsx>{`
         .container {
@@ -44,23 +74,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          text-align: center;
-          padding-bottom: 8rem;
-        }
       `}</style>
 
       <style jsx global>{`
@@ -68,6 +81,7 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
+          background-color: #111111;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
